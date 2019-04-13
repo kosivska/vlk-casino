e4450b14cbea573e40df75808fec777a572c3620
kosivska <vikysya_ua_koss@ukr.net>
2019-04-13 15:37:49 +0300

    commit new

--- js/main.js
+++ js/main.js
@@ -13,3 +13,34 @@ $('.carousel').slick({
 		autoplay: false,
 		autoplaySpeed: 1300
 	});
+
+$('.carousel-mobile').slick({
+		dots: true,
+		infinite: true,
+		arrows: false,
+		autoplay: false,
+		autoplaySpeed: 1300
+	});
+
+$('.preview-mobile').slick({
+  centerMode: true,
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 1
+      }
+    }
+  ]
+});
